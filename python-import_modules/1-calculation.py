7fe428b0ab61e68ece73b82b6c86925157a888a9
Anman-nn <endricos@gmail.com>
2026-05-02 17:17:56 +0200

    task new

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+if __name__ == "__main__":
+    print("{} + {} = {}".fromat(add(a, b, add(a, b))))
+    print("{} - {} = {}".fromat(add(a, b, sub(a, b))))
+    print("{} * {} = {}".fromat(add(a, b, mul(a, b))))
+    print("{} / {} = {}".fromat(add(a, b, div(a, b))))
