4837ab800af780f1e78a4ea0432a9a29ea205e60
Anman-nn <endricos@gmail.com>
2026-05-03 15:40:37 +0200

    task new

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+sys.argv
+if len(sys.argv) < 2:
+    print("0 arguments.")
+elif len(sys.argv) == 2:
+    print("1 argument:\n" \
+    "1:", sys.argv[1])
+else:
+    print("{} arguments:".format(len(sys.argv) - 1))
+    for n in range(1, len(sys.argv)):
+        print("{}: {}".format(n, sys.argv[n]))
