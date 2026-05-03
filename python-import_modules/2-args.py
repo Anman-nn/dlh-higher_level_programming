5ef638f3c339d8d68bdd14e96e8574bca4355962
Anman-nn <endricos@gmail.com>
2026-05-03 15:42:52 +0200

    task new

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-sys.argv
-if len(sys.argv) < 2:
-    print("0 arguments.")
-elif len(sys.argv) == 2:
-    print("1 argument:\n" \
-    "1:", sys.argv[1])
-else:
-    print("{} arguments:".format(len(sys.argv) - 1))
-    for n in range(1, len(sys.argv)):
-        print("{}: {}".format(n, sys.argv[n]))
+if __name__ == "__main__":
+    import sys
+    sys.argv
+    if len(sys.argv) < 2:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:\n" \
+        "1:", sys.argv[1])
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for n in range(1, len(sys.argv)):
+            print("{}: {}".format(n, sys.argv[n]))
