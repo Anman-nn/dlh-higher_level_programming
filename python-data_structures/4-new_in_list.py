bdc006831bed2100784157c250137d7346fb2f34
Anman-nn <endricos@gmail.com>
2026-05-05 21:51:15 +0200

    task new

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    my_new_list = my_list.copy()
+    if idx < 0 or (idx+1) > len(my_list):
+        return my_list
+    else:
+        my_new_list[idx] = element
+        return my_new_list
