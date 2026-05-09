785f15f7a6577c4ef88d015e93f31a39e8e1524a
Anman-nn <endricos@gmail.com>
2026-05-09 19:57:19 +0200

    task new

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     """deletes the item at a specific position in a list"""
-    my_list = my_list[:idx] + my_list[idx+1:]
+    my_list[:] = my_list[:idx] + my_list[idx+1:]
     return my_list
