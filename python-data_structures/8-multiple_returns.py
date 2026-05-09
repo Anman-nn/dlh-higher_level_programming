b010b1a1de9f20377fd5c1b8f25cb29bfdf82557
Anman-nn <endricos@gmail.com>
2026-05-09 17:29:08 +0200

    task new

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    return (len(sentence), sentence[0])
+    first_char = None
+    if sentence:
+        first_char = sentence[0]
+    return (len(sentence), first_char)
