2fa831942810a86107e7d235aaa650703bbee300
Anman-nn <endricos@gmail.com>
2026-05-09 17:17:52 +0200

    task new

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,8 +2,8 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     list_a = list(tuple_a)[:2]
     list_b = list(tuple_b)[:2]
-    if len(list_a)<2:
+    while len(list_a) < 2:
         list_a.append(0)
-    if len(list_b)<2:
+    while len(list_b) < 2:
         list_b.append(0)
     return tuple(map(sum, zip(list_a, list_b)))
