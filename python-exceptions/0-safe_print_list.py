8524562d88cd3812e2ff29ca3b0d0ecaf5842b9d
Anman-nn <endricos@gmail.com>
2026-05-14 17:10:21 +0200

    task new

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    new_str = ""
+    n = 0
+    while n < x:
+        try:
+            new_str += str(my_list[n])
+        except:
+            break
+        n += 1
+    try:
+        return int(new_str)
+    except:
+        return None
+
+
+
