be985d5211f8cc876d48bb0025e6e30fdd75965d
Anman-nn <endricos@gmail.com>
2026-05-14 17:23:35 +0200

    task new

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,6 +4,7 @@ def safe_print_list(my_list=[], x=0):
     n = 0
     while n < x:
         try:
+            int(my_list[n])
             new_str += str(my_list[n])
         except:
             break
@@ -11,5 +12,5 @@ def safe_print_list(my_list=[], x=0):
     try:
         print(int(new_str))    
     except:
-        return None
+        return 0
     return n
