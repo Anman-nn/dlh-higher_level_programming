c3ed1e6bc8d792723142381e1141c8cb4d7145f0
Anman-nn <endricos@gmail.com>
2026-05-14 17:48:25 +0200

    task new

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,10 +2,12 @@
 def safe_print_list_integers(my_list=[], x=0):
     new_str = ""
     n = 0
+    m = 0
     while n < x:
         try:
             int(my_list[n])
             new_str += str(my_list[n])
+            m += 1
         except:
             pass
         n += 1
@@ -13,4 +15,4 @@ def safe_print_list_integers(my_list=[], x=0):
         print("{:d}".format(int(new_str)))    
     except:
         return 0
-    return n
+    return m
