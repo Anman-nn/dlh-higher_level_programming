abdf5f3d2d9594a8ad001e56cde35565a7310e1a
Anman-nn <endricos@gmail.com>
2026-05-14 17:56:04 +0200

    task new

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    new_str = ""
     n = 0
+    count = 0
     while n < x:
         try:
-            int(my_list[n])
-            new_str += str(my_list[n])
-        except:
+            print("{:d}".format(my_list[n]), end="")
+            count += 1
+        except (ValueError, TypeError):
             pass
         n += 1
-    try:
-        print(int(new_str))    
-    except:
-        return 0
-    return n
+    print()
+    return count
