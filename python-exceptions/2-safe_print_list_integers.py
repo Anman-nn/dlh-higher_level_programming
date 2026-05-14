f03043bdf7680f116bb200858da35b69dbd76fd6
Anman-nn <endricos@gmail.com>
2026-05-14 17:35:12 +0200

    task new

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    new_str = ""
+    n = 0
+    while n < x:
+        try:
+            int(my_list[n])
+            new_str += str(my_list[n])
+        except:
+            pass
+        n += 1
+    try:
+        print("{:d}".format(int(new_str)))    
+    except:
+        return 0
+    return n
