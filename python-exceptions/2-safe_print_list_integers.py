67afafb4a46a9cc50dc8c8c8f41b59f6d0ac5a0d
Anman-nn <endricos@gmail.com>
2026-05-14 17:54:37 +0200

    task new

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    new_str = ""
     n = 0
-    m = 0
+    count = 0
     while n < x:
         try:
-            int(my_list[n])
-            new_str += str(my_list[n])
-            m += 1
-        except:
+            print("{:d}".format(my_list[n]), end="")
+            count += 1
+        except (ValueError, TypeError):
             pass
         n += 1
-    try:
-        print("{:d}".format(int(new_str)))    
-    except:
-        return 0
-    return m
+    print()
+    return count
