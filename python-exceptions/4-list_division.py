8bee3a28a4e0a0b65b81c5d0828573019496d142
Anman-nn <endricos@gmail.com>
2026-05-16 22:29:27 +0200

    task new

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -11,5 +11,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print('out of range')
         except TypeError:
             print('wrong type')
+        finally:
+            pass
         res.append(c)
     return res
