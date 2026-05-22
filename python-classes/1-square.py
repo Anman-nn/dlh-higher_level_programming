bec3715f67a463004eaf02d6af3efd4f31837a8a
Anman-nn <endricos@gmail.com>
2026-05-22 14:45:32 +0200

    task new

---
+++ python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for class Square"""
+class Square:
+    """Defines a square"""
+    def __init__(self, size):
+        """This is _init_ function"""
+        self.__size = size
