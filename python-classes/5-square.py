2275bfbe0ebb46620ecdee36e253ee1dfc504711
Anman-nn <endricos@gmail.com>
2026-05-23 19:45:40 +0200

    task new

---
+++ python-classes/5-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module for class Square"""
+class Square:
+    """Defines a square class"""
+    def __init__(self, size=0):
+        """This is _init_ function"""
+        self.size = size
+    @property
+    def size(self):
+        """This is getter for Size attribute"""
+        return self.__size
+    @size.setter
+    def size(self, value):
+        """This is setter to use: size = x"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    def area(self):
+        """returns the current square area"""
+        return self.__size ** 2
+    def my_print(self):
+        """Prits square with #"""
+        if self.size = 0:
+            print('')
+        else:
+            for _ in range(self.size):
+                print("#" * self.size)
