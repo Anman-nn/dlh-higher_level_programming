65ac8b6e0bac14f0bd82374f37fefcbd2101efda
Anman-nn <endricos@gmail.com>
2026-05-24 16:24:31 +0200

    task new

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Module for class Square"""
+
+
 class Square:
     """Defines a square"""
     pass
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Defines a square."""
+"""Defines a square. Defines a square."""
 
 
 class Square:
-    """Represents a square."""
+    """Represents a square. Defines a square."""
  
     def __init__(self, size):
         """Initialize square with private size."""
