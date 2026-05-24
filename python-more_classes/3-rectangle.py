7cc7127444eb1db2001685c6cfdd11d0d81161bc
Anman-nn <endricos@gmail.com>
2026-05-24 17:45:54 +0200

    task new

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -36,9 +36,15 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        return self.__width * self.__height
+        return self.width * self.height
 
     def perimeter(self):
-        if self.__width * self.__height == 0:
+        if self.width * self.height == 0:
             return 0
-        return (self.__width + self.__height) * 2
+        return (self.width + self.height) * 2
+
+    def __str__(self):
+        rec = ""
+        for _ in range(self.height):
+            rec += "#" * self.width + "\n"
+        return rec[:-1]
