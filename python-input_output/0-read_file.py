28442cd33e10b67a857387cb0e180796a2fef2f8
Anman-nn <endricos@gmail.com>
2026-05-25 17:11:36 +0200

    task new

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Module: learning input - output"""
+
 def read_file(filename=""):
+    """Function reads the file and prints it"""
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
     print (read_data)
