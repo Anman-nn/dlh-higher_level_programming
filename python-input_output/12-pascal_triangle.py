c8862594deb4c5cd966d129c02c5fb2aa7a2102b
Anman-nn <endricos@gmail.com>
2026-05-30 19:27:32 +0200

    task new

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module for Pascal"""
+
+
+def pascal_triangle(n):
+
+    if n <= 0:
+        return []
+
+    if n == 1:
+        return [1]
+    res = [[1], [1, 1]]
+
+    if n == 2:
+        return res
+
+    for row in range(1, n-1):
+        prev_row = res[row]
+        new_row = [1]
+        for n in range(len(prev_row)-1):
+            new_row.append(prev_row[n] + prev_row[n+1])
+            new_row.append(1)
+            res.append(new_row)
+    return res
