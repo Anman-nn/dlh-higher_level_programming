57beaaae11e3cb9b35cc15d19dc7d0b7b0a4338d
Anman-nn <endricos@gmail.com>
2026-05-30 18:31:50 +0200

    task new

---
+++ python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Module for a Class - Student"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+
+        if not isinstance(attrs, list):
+            return self.__dict__
+
+        for a in attrs:
+            if not isinstance(a, str):
+                return self.__dict__
+
+        res = {}
+        for a in attrs:
+            try:
+                res[a] = self.__dict__[a]
+            except KeyError:
+                continue
+        return res
+
+    def reload_from_json(self, json):
+        import json
+        data = json.loads(json)
+        for a, b in data:
+            self.__dict__[a] = b
