781400cadb868fe64c316310f62da58c8d8a53c4
Anman-nn <endricos@gmail.com>
2026-05-30 16:56:42 +0200

    task new

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,5 +5,5 @@
 def save_to_json_file(my_obj, filename):
     """Function to_json_string"""
     import json
-    with open(filename, encoding='utf-8') as f:
+    with open(filename, 'w', encoding='utf-8') as f:
         f.write(json.dumps(my_obj))
